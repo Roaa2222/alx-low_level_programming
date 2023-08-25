f374916faaa1f0712a90e382380c1fef0a40b4e5
Roaa2222 <me@example.com>
2023-08-25 11:02:07 -0700

    code

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _sqrt - finds the square root
+ *
+ * @x: input number
+ *
+ * Return: square root of x
+*/
+
+double -sqrt(double x)
+{
+float sqrt, tmp;
+
+sqrt = x / 2
+	
